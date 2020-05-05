671dc9a3d7f2681d526dc76147110426971150cc
Nelio Santos <nelio.santos@jbtec.com.br>
2020-05-05 20:01:11 -0300

    feat: adding a tool for read spiffs

--- tools/readspiffs.sh
+++ tools/readspiffs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# Retrieve from ESP8266 if is needed
 if [ ! -f spiffs.bin ] ; then
-    esptool.py --port $ESPPORT read_flash 884736 163840  myspiffs.bin
+    esptool.py --port $ESPPORT read_flash 884736 163840 spiffs.bin
 fi
 
+# Remove old files
 if [ -d spiffs ] ; then
     rm -rf spiffs
 fi
-mkdir spiffs
-mkspiffs/mkspiffs -d 5 -p 256 -u myspiffs spiffs.bin
 
-cat myspiffs/config.json
+# Extract the content
+mkdir spiffs
+mkspiffs/mkspiffs -p 256 -u spiffs spiffs.bin
