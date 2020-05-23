680f0f47b3ffb92b9296fce9c91d1f1a4d3a38b0
Nelio Santos <nsfilho@icloud.com>
2020-05-23 11:21:22 -0300

    adding actions for autobuild

--- .github/actions/esp8266-rtos-sdk/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -f ota/certs/ca_cert.pem ] ; then
-    bash ota/certs/generate.sh
-fi
-
-make defconfig all
--- main/e12aio.h
+++ main/e12aio.h
@@ -20,4 +20,4 @@
  */
 #pragma once
 
-#define E12AIO_VERSION "2.1.6"
+#define E12AIO_VERSION "2.1.7"
